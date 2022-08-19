6e2cdf51ae58d04ea74b4adaf0b61d4ccf0387a1
Emiliano <russo.emiliano@gmail.com>
2022-08-19 15:55:26 -0300

    login

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
 const express = require("express");
+const jwt = require("jsonwebtoken");
+const bodyParser = require("body-parser");
+const config = require("./config.json");
 const app = express();
 
-app.listen(3010);
+app.use(bodyParser.json());
+app.listen(config.port);
 
-app.post("/login", (req, res) => {
-  console.log("login here!");
-  res.send("Take the token: oias9812j");
+app.post(config.loginRoute, (req, res) => {
+  const user = req.body;
+  jwt.sign(user, "secretKey", function name(err, token) {
+    res.json({
+      token,
+    });
+  });
+});
+
+app.get(config.userInfo, (req, res) => {
+  console.log("user info");
 });
