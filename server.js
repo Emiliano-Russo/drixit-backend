7e315afb70aa2d8f0cedddff75dc366754a834bf
Emi <russo.emiliano97@gmail.com>
2022-08-21 00:20:00 -0300

    cleaning

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
+require("dotenv").config();
 const express = require("express");
 const jwt = require("jsonwebtoken");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const config = require("./config.json");
+const config = require("./routes.json");
 const db = require("./users");
 const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
-app.listen(config.port);
+app.listen(process.env.PORT);
 
 function userExists(user) {
   return db.users.find((u) => u.email == user.email && u.password == user.password);
@@ -27,6 +28,5 @@ app.post(config.loginRoute, (req, res) => {
 app.get(config.userInfo, (req, res) => {
   const token = req.query.token;
   const user = jwt.decode(token);
-  if (!userExists(user)) res.status(401).send("invalid token");
-  else res.send(user);
+  return userExists(user) ? res.send(user) : res.status(401).send("invalid token");
 });
