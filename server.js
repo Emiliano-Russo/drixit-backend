2f2adbfb52ff0ae6a20be50c01ac17d78f442087
Emi <russo.emiliano97@gmail.com>
2022-08-20 21:41:16 -0300

    users and errores implemented

--- server.js
+++ server.js
@@ -3,14 +3,21 @@ const jwt = require("jsonwebtoken");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const config = require("./config.json");
+const db = require("./users");
 const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
 app.listen(config.port);
 
+function userExists(user) {
+  return db.users.find((u) => u.email == user.email && u.password == user.password);
+}
+
 app.post(config.loginRoute, (req, res) => {
   const user = req.body;
+  console.log("user:", user);
+  if (!userExists(user)) res.status(401).send("failed login");
   jwt.sign(user, "secretKey", function name(err, token) {
     res.json({
       jwt: token,
@@ -20,6 +27,7 @@ app.post(config.loginRoute, (req, res) => {
 
 app.get(config.userInfo, (req, res) => {
   const token = req.query.token;
-  const result = jwt.decode(token);
-  res.send(result);
+  const user = jwt.decode(token);
+  if (!userExists(user)) res.status(401).send("invalid token");
+  else res.send(user);
 });
